5cdaf9b8464f7312c8edef13975c5951976aa2f1
hereticpride <allensmalls32@gmail.com>
2025-04-03 18:49:39 -0500

    I drew a triangle on the screen

--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,20 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 
+    framerate = 60
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    spawn_x = SCREEN_WIDTH / 2
+    spawn_y = SCREEN_HEIGHT / 2
+    player = Player(spawn_x, spawn_y)
 
     while True:
         for event in pygame.event.get():
@@ -14,7 +22,9 @@ def main():
                 return
             
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
    
     
 if __name__ == "__main__":
